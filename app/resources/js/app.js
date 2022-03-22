9cf12f6f997ae29f9f4599ec53c685b089db0b58
yu-v-m123 <nagase.yuki0123@gmail.com>
2022-03-23 00:58:46 +0900

    add vue-router

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -1,8 +1,13 @@
 require('./bootstrap');
 
-import Vue from 'vue';
+import Vue from 'vue'
+import router from './router'
+import App from './App.vue'
 
 new Vue({
   el: '#app',
-  template: '<h1>Hello World</h1>'
+  router,
+  // components: { App },
+  // template: '<App />'
+  render: h => h(App)
 })
---
+++ app/resources/js/router.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import PhotoList from './pages/PhotoList.vue'
+import Login from './pages/Login.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    component: PhotoList 
+  },
+  {
+    path: '/login',
+    component: Login
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
