5ba20a80423f65f5c67c54532b2c9cc07e57e57c
yu-v-m123 <nagase.yuki0123@gmail.com>
2022-03-25 01:45:32 +0900

    install Vuex

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -3,11 +3,11 @@ require('./bootstrap');
 import Vue from 'vue'
 import router from './router'
 import App from './App.vue'
+import store from './store'
 
 new Vue({
   el: '#app',
   router,
-  // components: { App },
-  // template: '<App />'
+  store,
   render: h => h(App)
 })
---
+++ app/resources/js/store/auth.js
@@ -0,0 +1,15 @@
+const state = {}
+
+const geeters = {}
+
+const mutations = {}
+
+const actions = {}
+
+export default {
+  namespaced: true,
+  state,
+  geeters,
+  mutations,
+  actions
+}
