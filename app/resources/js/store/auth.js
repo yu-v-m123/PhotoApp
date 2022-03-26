6ff9997cca590fa27c6b7d71e98a885af3ab876d
yu-v-m123 <nagase.yuki0123@gmail.com>
2022-03-27 02:04:47 +0900

    add auth state

--- app/resources/js/store/auth.js
+++ app/resources/js/store/auth.js
@@ -1,10 +1,25 @@
-const state = {}
+import Axios from "axios"
 
-const geeters = {}
+const state = {
+  user: null
+}
 
-const mutations = {}
+const geeters = {
+  
+}
 
-const actions = {}
+const mutations = {
+  setUser(state, user) {
+    state.user = user
+  }
+}
+
+const actions = {
+  async registers(context, data) {
+    const response = await axios.post('/api/register', data)
+    context.commit('setUser', response.data)
+  }
+}
 
 export default {
   namespaced: true,
